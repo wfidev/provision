e9738b33458fa990d091c5aec33b39adbd82a7a2
paul <dev@wfi>
2023-06-06 17:28:05 -0600

    default coding environment

--- ubuntu-arm/initial-setup.sh
+++ ubuntu-arm/initial-setup.sh
@@ -22,3 +22,17 @@ sudo apt install snapd -y
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Set up a default coding environment
+#
+mkdir ~/Code
+sudo apt install python3.10-venv -y
+python3 -m venv 2305
+pushd .
+cd ~/Code
+git clone https://github.com/wfidev/alias
+cp alias/.bash_aliases
+popd
+echo "Restart bash to import new aliases"
+
+
